b32048ba6598ca01c6bc9ca5d15a256fca6a4f2b
Lina Sachuk <sachuklina@gmail.com>
2020-07-15 23:05:08 -0600

    add index.js

--- Learning TensorFlow with JavaScript/index.js
+++ Learning TensorFlow with JavaScript/index.js
@@ -1,1 +1,19 @@
-console.log('It works!')
+// shape our tenser with 4 rows of 2 columns
+const shape = [4, 2]
+// const data = tf.tensor([[4, 6], [5, 9], [12, 25], [1, 57]])
+
+// feed data into the tensor
+const data = tf.tensor([4, 6, 5, 9, 12, 25, 1, 57], shape)
+
+// set variables with zeros method
+const data2 = tf.variable(tf.zeros([8]));
+
+
+
+// print the data
+data.print()
+data2.print()
+
+// this is where we assign new values with 1 dimension
+data2.assign(tf.tensor1d([4, 25, 5, 56, 3, 45, 4, 6]));
+data2.print()
