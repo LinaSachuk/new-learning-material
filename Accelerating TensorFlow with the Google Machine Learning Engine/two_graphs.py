4e61cf4167467e57712e81854ff57b732f1054d7
Lina Sachuk <sachuklina@gmail.com>
2020-07-24 23:32:48 -0600

    add final code for the two_graphs.py

--- Accelerating TensorFlow with the Google Machine Learning Engine/two_graphs.py
+++ Accelerating TensorFlow with the Google Machine Learning Engine/two_graphs.py
@@ -17,7 +17,12 @@ graph2 = tf.Graph()
 with graph2.as_default():
 
     # Step 3: Create two tensors in the second graph and a subtraction operation
-
+    t4 = tf.constant([5.6, 6.7, 7.8, 8.9])
+    t5 = tf.random_normal([4])
+    t6 = t4 - t5
     # Step 4: Create a session and execute the addition operation from the first graph
-
+with tf.Session(graph=graph1) as sess:
+    print('Addition', sess.run(t3))
     # Step 5: Create a second session and execute the subtraction operation from the second graph
+with tf.Session(graph=graph2) as sess:
+    print('Subtruction', sess.run(t6))
